df828e0f2bdc625fc01c2dcbbcc0fbf038cf975c
Bohdan Sulyha <anonimoos13@gmail.com>
2021-08-18 19:25:09 +0300

    'first'

---
+++ main.js
@@ -0,0 +1,22 @@
+// let user = {
+//     name: 'Ivan',
+//     age: 22,
+//     showInfo(param1, param2){
+//         console.log(param1, param2)
+//         console.log(`name: ${this.name}, age: ${this.age}`);
+//     }
+// }
+
+// user.showInfo();
+
+// let person = {
+//     name: 'Petro',
+//     age: 33,
+// };
+
+// let showInfo = user.showInfo.bind(person);
+// showInfo();
+
+// user.showInfo.call(person, 'qq', 'zz');
+// user.showInfo.call(person, ['qq', 'zz']);
+
